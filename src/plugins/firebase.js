6c95817df35481e85eba5866ef21e0f383c1cc39
Petzi <daboarderpjb@gmail.com>
2020-09-28 18:43:37 +0200

    image upload läuft

---
+++ src/plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAQrUZ44HnkaVksAqUs4G79TiaG1Ejwiw0",
+    authDomain: "documentation-dfdb2.firebaseapp.com",
+    databaseURL: "https://documentation-dfdb2.firebaseio.com",
+    projectId: "documentation-dfdb2",
+    storageBucket: "documentation-dfdb2.appspot.com",
+    messagingSenderId: "644597680458",
+    appId: "1:644597680458:web:b26f46fb441a0a55b799b3",
+    measurementId: "G-Y9ZR7M799S"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp.firestore();
--- src/views/SaveRecipe.vue
+++ src/views/SaveRecipe.vue
@@ -5,41 +5,162 @@
         <v-card-title>
           Save New Recipe
         </v-card-title>
-        <v-form ref="form">
+        <v-card-subtitle>
+          {{ user.email }}
+        </v-card-subtitle>
+        <v-form ref="form" @submit.prevent="saveFile">
           <v-text-field label="Rezept Name" v-model="recipeName"></v-text-field>
+          <v-row>
+            <v-col cols-12>
+              <v-btn outlined class="primary" @click="onPickFile" dark
+                ><v-icon left>mdi-camera-outline</v-icon> Image</v-btn
+              >
+              <input
+                class="caption ma-2"
+                v-show="false"
+                contenteditable="false"
+                type="file"
+                prepend-icon="mdi-camera"
+                ref="fileInput"
+                @change="onFilePicked"
+              />
+              <span v-if="image"> {{ filename }}</span
+              ><v-btn text>X</v-btn>
+            </v-col>
+          </v-row>
+          <v-btn block tile large elevation="5" type="submit">Speichern</v-btn>
         </v-form>
       </v-card>
-      <v-list>
-        <v-list-group
-          :value="false"
-          prepend-icon="mdi-account-circle"
-          no-action
-        >
-          <template v-slot:activator>
-            <v-list-item-title>
-              Titel
-            </v-list-item-title>
-          </template>
-
-          <v-list-item link v-for="n in 3" :key="n">
-            <v-list-item-content>
-              <v-list-item-title> {{ n }} Child Title </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-group>
-      </v-list>
+      {{ image }}
     </v-col>
   </v-row>
 </template>
 
 <script>
+import db from "@/plugins/firebase";
+import * as firebase from "firebase";
+
 export default {
   name: "SaveRecipe",
   data: () => ({
-    recipeName: "name"
+    recipeName: "",
+    imgsrc: "",
+    image: null,
+    filename: null,
+    user: null,
+    savedRecipe: null
   }),
-  methods: {},
-  computed: {}
+  methods: {
+    onPickFile() {
+      this.$refs.fileInput.click();
+    },
+    logIn() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("test@test.de", "testtest")
+        .then(res => {
+          this.user = res.user;
+          console.log("eingeloggt", this.user.email);
+        });
+    },
+    saveFile() {
+      console.log("image: ", this.image);
+      console.log("filename: ", this.filename);
+      console.log("imgsrc: ", this.imgsrc);
+      const file = this.image;
+      console.log(file);
+
+      let input = {
+        recipeName: this.recipeName,
+        imageName: this.filename,
+        time: Date.now()
+      };
+      //const metadata = { contentType: "image/jpeg" }
+      db.collection("recipes")
+        .add(input)
+        .then(res => {
+          let key = res.id;
+          console.log("key:", key);
+          return key;
+        })
+        .then(key => {
+          let ext = input.imageName.slice(input.imageName.lastIndexOf("."));
+          console.log("ext", ext)
+          return firebase
+            .storage()
+            .ref("recipes/" + key + ext)
+            .put(file)
+            .then(URL => {
+              console.log("hochgeladen", URL);
+            })
+            .catch(error => {
+              console.error("Fehler: ", error);
+            });
+        });
+    },
+    sendInput() {
+      let input = {
+        name: this.recipeName,
+        imageName: this.filename,
+        time: Date.now()
+      };
+      let imgsrc;
+      let key;
+      let ext;
+      db.collection("recipes")
+        .doc("recipe-" + this.recipeName)
+        .set(input)
+        .then(data => {
+          console.info(data.data.key);
+          key = data.data.key;
+          return key;
+        })
+        .then(key => {
+          const filename = this.image.name;
+          ext = filename.slice(filename.lastIndexOf("."));
+          return firebase
+            .storage()
+            .ref("recipes/" + key + ext)
+            .put(this.image);
+        })
+        .then(URL => {
+          imgsrc = URL;
+          console.log(imgsrc);
+          return db
+            .collection("recipes")
+            .child(key)
+            .update({ imgsrc: imgsrc });
+        })
+        .then(() => {
+          this.savedRecipe = {
+            id: key,
+            link: "recipe/" + key,
+            imgsrc: imgsrc,
+            ...input
+          };
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    onFilePicked(event) {
+      const files = event.target.files;
+      this.filename = files[0].name;
+      if (this.filename.lastIndexOf(".jpg") <= 0) {
+        return alert("Falsch!");
+      }
+      const fileReader = new FileReader();
+      fileReader.addEventListener("load", () => {
+        this.imgsrc = fileReader.result;
+      });
+      fileReader.readAsDataURL(files[0]);
+      this.image = files[0];
+    }
+  },
+  computed: {},
+  mounted() {
+    this.logIn();
+  }
 };
 </script>
 
