7c99145a5e24640c74df4c89e05dadae14ef3944
Petzi <daboarderpjb@gmail.com>
2021-11-21 21:45:41 +0100

    update package.json

--- src/views/UploadImage.vue
+++ src/views/UploadImage.vue
@@ -11,10 +11,17 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="6" sm="3" md="2" lg="1" v-for="(b,i) in imageGallerie" :key="i">
-       <v-card class="pa-0 ma-0">
-           <v-img :src="b" class="mx-auto" height="100px"></v-img>
-       </v-card>
+      <v-col
+        cols="6"
+        sm="3"
+        md="2"
+        lg="1"
+        v-for="(b, i) in imageGallerie"
+        :key="i"
+      >
+        <v-card class="pa-0 ma-0">
+          <v-img :src="b" class="mx-auto" height="100px"></v-img>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -36,12 +43,12 @@ export default {
     }
   },
   computed: {
-    imageGallerie(){
+    imageGallerie() {
       return this.$store.state.gallerie;
     }
   },
   beforeMount() {
-    this.$store.dispatch("loadImages")
+    this.$store.dispatch("loadImages");
   }
 };
 </script>
