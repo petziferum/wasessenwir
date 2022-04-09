6caad9f947c631811687936c1235bc938605f7db
Petzi <daboarderpjb@gmail.com>
2022-04-09 10:50:40 +0200

    rezept menue

--- src/components/authentication/UserDashboard.vue
+++ src/components/authentication/UserDashboard.vue
@@ -14,51 +14,100 @@
         <v-btn small text @click="updateUser">Update User</v-btn>
       </v-card-subtitle>
       <template v-if="userData">
-        <v-card-text>
-          <v-text-field label="ID" :value="userData.id" readonly></v-text-field>
-          <v-text-field
-            label="User Name"
-            :value="userData.userName"
-            readonly
-          ></v-text-field>
-          <v-text-field
-            label="lastLogin"
-            :value="Date(userData.lastLogin)"
-            readonly
-          ></v-text-field>
-          <v-text-field
-            label="email"
-            :value="userData.email"
-            readonly
-          ></v-text-field>
-          <v-text-field
-            label="recipes"
-            :value="userData.recipes"
-            readonly
-          ></v-text-field>
-          <v-text-field
-            label="firstName"
-            v-model="userData.firstName"
-          ></v-text-field>
-          <v-text-field
-            label="lastName"
-            v-model="userData.lastName"
-          ></v-text-field>
-        </v-card-text>
+        <v-row>
+          <v-col cols="6">
+            <v-card-text>
+              <v-text-field
+                label="ID"
+                :value="userData.id"
+                readonly
+              ></v-text-field>
+              <v-text-field
+                label="User Name"
+                :value="userData.userName"
+                readonly
+              ></v-text-field>
+              <v-text-field
+                label="lastLogin"
+                :value="Date(userData.lastLogin)"
+                readonly
+              ></v-text-field>
+              <v-text-field
+                label="email"
+                :value="userData.email"
+                readonly
+              ></v-text-field>
+              <v-text-field
+                label="recipes"
+                :value="userData.recipes"
+                readonly
+              ></v-text-field>
+              <v-text-field
+                label="firstName"
+                v-model="userData.firstName"
+              ></v-text-field>
+              <v-text-field
+                label="lastName"
+                v-model="userData.lastName"
+              ></v-text-field>
+            </v-card-text>
+          </v-col>
+          <v-col cols="6">
+            <v-card>
+              <v-card-title>
+                Benutzerrezepte
+              </v-card-title>
+              <v-card-subtitle>
+                <v-btn small @click="getUserRecipes">Rezepte laden</v-btn>
+              </v-card-subtitle>
+              <v-card-text>
+                <v-list>
+                  <v-list-group v-for="r in userRecipes" :key="r.id">
+                    <template v-slot:activator>
+                      <v-list-item-content>
+                        <v-list-item-title>{{
+                          r.recipeName
+                        }}</v-list-item-title>
+                      </v-list-item-content>
+                    </template>
+                    <v-card rounded="xl">
+                      <v-img :src="r.imageSrc" :height="100">
+                        <v-app-bar flat color="rgba(0, 0, 0, 0.5)">
+                          <v-menu offset-y>
+                            <template v-slot:activator="{ on }">
+                              <v-app-bar-nav-icon
+                                color="white"
+                                v-on="on"
+                              ></v-app-bar-nav-icon>
+                            </template>
+                            <v-card>
+                              <v-card-text>{{ r }}</v-card-text>
+                            </v-card>
+                          </v-menu>
+
+                          <v-toolbar-title class="text-h6 white--text pl-0">
+                            {{ r.recipeName }}
+                          </v-toolbar-title>
+
+                          <v-spacer></v-spacer>
+                        </v-app-bar>
+                      </v-img>
+                      <v-card-text>
+                        <div v-for="d in r.recipeDescription" :key="d.nr">
+                          {{ d.text }}
+                        </div>
+                      </v-card-text>
+                    </v-card>
+                  </v-list-group>
+                </v-list>
+              </v-card-text>
+            </v-card></v-col
+          >
+        </v-row>
       </template>
     </v-card>
     <!-- Rezepte des Benutzers -->
-    <v-card class="mt-4">
-      <v-card-title>
-        Benutzerrezepte
-      </v-card-title>
-      <v-card-subtitle>
-        <v-btn small @click="getUserRecipes">Rezepte laden</v-btn>
-      </v-card-subtitle>
-      <v-card-text>
-        <div v-for="r in userRecipes" :key="r.id">{{ r.recipeName }}</div>
-      </v-card-text>
-    </v-card>
+    <v-card class="mt-4"> </v-card>
   </v-container>
 </template>
 
