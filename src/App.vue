669177bc4cbe1d15d364a0985174344b66f7c1c0
Petzi <daboarderpjb@gmail.com>
2022-10-14 10:47:03 +0200

    updating round 2

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,10 @@
             :key="item.title"
           >
             <v-list-item-icon
-              ><v-icon v-text="item.action"></v-icon
+              ><v-icon>{{ item.action }}</v-icon
             ></v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
